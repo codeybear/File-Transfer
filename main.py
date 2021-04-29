bca10cdd6dc490cd0d5f317c312aec186c2fdb03
Peter C <30731499+codeybear@users.noreply.github.com>
2021-04-29 13:19:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sync
 import sys
 import web
+import exceptions
 
 folder = sys.argv[1]
 scanner = sync.DirScanner(folder, 100)
 newfiles = scanner.scan()
 
+# this needs to be put into some kind of function, maybe a class which gets the checksum and remembers stuff
 for file in newfiles:
-    print(file)
-    web.SendFile("http://127.0.0.0.1/sendfile", "file")
+    print(f"{file}")
+
+    try:
+        web.SendFile("http://127.0.0.0.1/sendfile", file)
+        print(f"File {file} sent to server.")
+    except exceptions.ConnectionError:
+        print("Unable to send file f{file}")
+    

7b02aa07d776ea6a0340f8e817729ca407f8f1f5
Peter C <30731499+codeybear@users.noreply.github.com>
2021-05-01 00:47:23 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+import sys
+import urllib
 
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -8,32 +11,42 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
     def _html(self, message):
-        """This just generates an HTML document that includes `message`
-        in the body. Override, or re-write this do do more interesting stuff.
-        """
         content = f"<html><body><h1>{message}</h1></body></html>"
-        return content.encode("utf8")  # NOTE: must return a bytes object!
-
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write(self._html("hi!"))
-
-    def do_HEAD(self):
-        self._set_headers()
+        return content.encode("utf8")
 
     def do_POST(self):
-        # Doesn't do anything with posted data
+        length = int(self.headers.get('content-length'))
+        field_data = self.rfile.read(length).decode('utf-8')
+        fields = urllib.parse.parse_qs(field_data)
+        filename, filedata = S.extract_file_details(fields)
+
+        with open(os.path.join(save_folder, filename), "w") as file:
+            file.write(filedata)
+
         self._set_headers()
         self.wfile.write(self._html("POST!"))
 
+    def extract_file_details(fields):
+        from os import linesep
 
-def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+        data = fields[' filename'][0]
+        filename = data[:data.find(linesep)].replace('"', '')
+        filedata = data[data.find(linesep * 2) + len(linesep * 2):]
+        filedata = filedata[:filedata.find("--")]
+        filedata = filedata[:filedata.rfind(linesep)]
+        filedata = filedata.replace("\r", "")
+
+        return filename, filedata
+
+
+def run(folder, server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
     server_address = (addr, port)
     httpd = server_class(server_address, handler_class)
+    global save_folder 
+    save_folder = folder
 
     print(f"Starting httpd server on {addr}:{port}")
     httpd.serve_forever()
 
-
 if __name__ == "__main__":
-    run()
+    run(sys.argv[1])

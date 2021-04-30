54c29db4c0f213e1896ae09a4beec06d5424ef34
Peter C <30731499+codeybear@users.noreply.github.com>
2021-04-30 13:34:11 +0100

    Client fixed

--- client.py
+++ client.py
@@ -9,8 +9,8 @@ newfiles = scanner.scan()
 # this needs to be put into some kind of function, maybe a class which gets the checksum and remembers stuff
 for file in newfiles:
     try:
-        web.SendFile("http://127.0.0.0.1/sendfile", file)
+        web.SendFile("http://localhost:8000/", file)
         print(f"File {file} sent to server.")
-    except:
+    except Exception as exc:
         print(f"Unable to send file {file} will retry later")
-    
+        print(f"Exception {exc}")
--- web.py
+++ web.py
@@ -3,4 +3,4 @@ import requests
 def SendFile(url, filename):
     with open(filename, 'rb') as f:
         req = requests.post(url, files={filename: f})
-        print(req.Response)
+        print(req.status_code, req.content)

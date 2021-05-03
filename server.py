7195fe757df242d4606fd7cde09dc1da0dc51e5e
Peter C <30731499+codeybear@users.noreply.github.com>
2021-05-03 12:53:21 +0100

    Refactoring

--- server.py
+++ server.py
@@ -4,43 +4,42 @@ import os
 import sys
 import config
 
-folder = sys.argv[1] 
-
-s = socket.socket()
-s.bind((config.SERVER_HOST, config.SERVER_PORT))
-s.listen(5)
-print(f"[*] Listening as {config.SERVER_HOST}:{config.SERVER_PORT}")
-
 def LoadHeader():
-    # receive using client socket, not server socket
+    """Load the filename from the first section of data."""
     received = client_socket.recv(config.BUFFER_SIZE)
     filename = received[:received.find(0)].decode()
     file_content = received[received.find(0)+1:]
 
     # Convert path to server folder
     filename = os.path.basename(filename)
-    filename = os.path.join(folder, filename)
-    print(f"Receiving {filename}")
 
     return filename, file_content
 
-while True:
-    client_socket, address = s.accept() 
-    print(f"[+] {address} is connected.")
 
-    # receive using client socket, not server socket
-    filename, file_content = LoadHeader()
+folder = sys.argv[1] 
 
-    with open(filename, "wb") as f:
-        f.write(file_content)
+with socket.socket() as s:
+    s.bind((config.SERVER_HOST, config.SERVER_PORT))
+    s.listen(5)
+    print(f"[*] Listening as {config.SERVER_HOST}:{config.SERVER_PORT}")
 
-        while True:
-            bytes_read = client_socket.recv(config.BUFFER_SIZE)
-            if not bytes_read:    
-                break
-            f.write(bytes_read)
-            print(f"{len(bytes_read)}")
+    while True:
+        # TODO need to make this a context manager, address is not used
+        client_socket, address = s.accept() 
+        print(f"[+] {address} is connected.")
 
-    client_socket.close()
-        
-s.close()
+        filename, file_content = LoadHeader()
+        filename = os.path.join(folder, filename)
+        print(f"Receiving {filename}")
+
+        with open(filename, "wb") as f:
+            f.write(file_content)
+
+            while True:
+                bytes_read = client_socket.recv(config.BUFFER_SIZE)
+                if not bytes_read:    
+                    break
+                f.write(bytes_read)
+                print(f"{len(bytes_read)}")
+
+        client_socket.close()

d345d8e5788c10f2310443935512eb51647e4c26
Peter C <30731499+codeybear@users.noreply.github.com>
2021-05-03 11:59:39 +0100

    First working version

--- server.py
+++ server.py
@@ -18,29 +18,31 @@ s.bind((SERVER_HOST, SERVER_PORT))
 # Accept up to 5 connections
 s.listen(5)
 print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-# accept connection if there is any
-client_socket, address = s.accept() 
-print(f"[+] {address} is connected.")
 
-# receive using client socket, not server socket
-received = client_socket.recv(BUFFER_SIZE) # 
-filename = received[:received.find(0)].decode()
-file_content = received[received.find(0)+1:]
+while True:
+    client_socket, address = s.accept() 
+    print(f"[+] {address} is connected.")
 
-# remove absolute path 
-filename = os.path.basename(filename)
-filename = os.path.join(folder, filename)
-print(f"Receiving {filename}")
+    # receive using client socket, not server socket
+    received = client_socket.recv(BUFFER_SIZE) # 
+    filename = received[:received.find(0)].decode()
+    file_content = received[received.find(0)+1:]
 
-with open(filename, "wb") as f:
-    f.write(file_content)
+    # remove absolute path 
+    filename = os.path.basename(filename)
+    filename = os.path.join(folder, filename)
+    print(f"Receiving {filename}")
 
-    while True:
-        bytes_read = client_socket.recv(BUFFER_SIZE)
-        if not bytes_read:    
-            break
-        f.write(bytes_read)
-        print(f"{len(bytes_read)}")
+    with open(filename, "wb") as f:
+        f.write(file_content)
 
-client_socket.close()
+        while True:
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                break
+            f.write(bytes_read)
+            print(f"{len(bytes_read)}")
+
+    client_socket.close()
+        
 s.close()
